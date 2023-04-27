3193ffccd4b27bb20e78a0f7b057f52ed7b26223
Vadim Shidlov <mat23vadim@mail.ru>
2023-04-27 14:07:30 +0300

    feat: create method getKeyboard

--- js/keyboard.js
+++ js/keyboard.js
@@ -6,4 +6,42 @@ class VirtualKeyboard extends Output {
     this.main = null;
     this.keyboard = null;
   }
+
+  getKeyboard(keyboardKeysLang) {
+    // create main-block
+    this.main = document.createElement("div");
+    this.main.classList.add("_container");
+    // create header
+    const header = document.createElement("header");
+    header.classList.add("header");
+    const headerTitle = document.createElement("h3");
+    headerTitle.textContent = "RS School Virtual Keyboard";
+    headerTitle.classList.add("header_title");
+    header.append(headerTitle);
+    this.main.append(header);
+    document.body.append(this.main);
+
+    // create keyboard
+    const bodyKeyboard = document.createElement("main");
+    bodyKeyboard.classList.add("page");
+    const sectionText = document.createElement("section");
+    sectionText.classList.add("text");
+    // const textArea = document.createElement("textarea");
+    // textArea.classList.add("text_area");
+    // textArea.innerText = this.output;
+    const textArea = this.element;
+    const sectionKeyboard = document.createElement("section");
+    sectionKeyboard.classList.add("keyboard");
+    // const sectionKeyboardItems = document.createElement("section");
+    this.sectionKeyboardItems = this.getKeyboardItems(
+      keyboardKeysLang,
+      this.shiftState
+    );
+    this.sectionKeyboardItems.classList.add("keyboard_items");
+    sectionText.append(textArea);
+    // sectionKeyboard.append(sectionKeyboardItems);
+    sectionKeyboard.append(sectionText);
+    sectionKeyboard.append(this.sectionKeyboardItems);
+    this.main.append(sectionKeyboard);
+  }
 }
